ee6c95ce34fa8474458f736fb8cbaf36547f51b3
zakjama0 <zakjama0@gmail.com>
2024-04-21 17:47:01 +0100

    Implemented shadcnui

--- blogapp/blognext/app/components/NavBar.tsx
+++ blogapp/blognext/app/components/NavBar.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function NavBar (){
     return (
-        <nav>
-            <h1> Hello from the navbar</h1>
+        <nav className ="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
+            <Link href="/" className="font-bold text-3xl">
+            Safar <span className="text-blue-500">Blog</span>
+            </Link>
         </nav>
     )
 }
