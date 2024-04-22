5203509ac982f76f5ce8f45d3565c072753f9f8e
zakjama0 <zakjama0@gmail.com>
2024-04-22 09:42:48 +0100

    testingcomming

--- blogapp/blognext/app/components/NavBar.tsx
+++ blogapp/blognext/app/components/NavBar.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
 
 export default function NavBar (){
     return (
         <nav className ="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
             <Link href="/" className="font-bold text-3xl">
-            Safar <span className="text-blue-500">Blog</span>
+            Safar <span className="text-primary">Blog</span>
             </Link>
+
+            <ModeToggle />
         </nav>
     )
 }
---
+++ blogapp/blognext/app/components/theme-provider.tsx
@@ -0,0 +1,10 @@
+"use client"
+// making sure that it knows its using the client rather than the server
+
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
